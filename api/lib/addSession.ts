588469875a3cfc78d61e6a70e77f1164ffd486bb
MasterZer0o <kubapod1234@wp.pl>
2022-07-02 23:48:37 +0200

    update adding session functionality

--- api/lib/addSession.ts
+++ api/lib/addSession.ts
@@ -3,25 +3,28 @@ import { Low, JSONFile } from 'lowdb';
 import { join, dirname } from 'path';
 import { Request, Response } from 'express';
 import type Session from '../Session';
-// addSession();
+
 export default async function addSession(req: Request, res: Response) {
-	res.header('Access-Control-Allow-Origin', '*');
+	try {
+		res.header('Access-Control-Allow-Origin', '*');
 
-	const session: Session = req.body;
-	console.log(session);
+		const session: Session = req.body;
 
-	res.json('success');
-	const __dirname = dirname(fileURLToPath(import.meta.url));
+		const __dirname = dirname(fileURLToPath(import.meta.url));
 
-	const file = join(__dirname, '../sessions.json');
-	const adapter = new JSONFile<any>(file);
-	const db = new Low(adapter);
+		const file = join(__dirname, '../sessions.json');
+		const adapter = new JSONFile<any>(file);
+		const db = new Low(adapter);
 
-	await db.read();
+		await db.read();
 
-	db.data ||= [];
-	console.log(db.data);
-	db.data.push(session);
+		db.data ||= [];
 
-	await db.write();
+		db.data.push(session);
+
+		await db.write();
+		res.json('success');
+	} catch (error) {
+		res.json({ error: { message: 'failed to add session' } });
+	}
 }
