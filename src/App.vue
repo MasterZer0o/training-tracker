2712134e23d14b1c9c28d26962751832a749ef5a
MasterZer0o <kubapod1234@wp.pl>
2022-07-02 23:55:25 +0200

    Update App.vue
    - add transition to showing sessions
    - add limited auth functionality

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-	<TheHeader />
-	<div class="container">
-		<Sessions />
-		<Error v-if="isError" :message="errorMessage" />
-	</div>
+	<TheHeader v-if="securityCheckPassed" />
+	<transition name="showUp">
+		<div v-if="securityCheckPassed" class="container">
+			<Sessions />
+			<Error v-if="isError" :message="errorMessage" />
+		</div>
+	</transition>
+	<SecurityBox :authError="authError" @requestAuth="handleAuth" v-if="!securityCheckPassed" />
 </template>
 
 <script setup lang="ts">
+import { defineAsyncComponent, onMounted, ref } from 'vue';
+import { BASE_URL } from './cfg';
 import { isError, errorMessage } from './store';
+const securityCheckPassed = ref(false);
+const SecurityBox = defineAsyncComponent(() => import('./components/Security.vue'));
+const authError = ref<{
+	is: boolean;
+	message: string;
+}>({ is: false, message: '' });
+onMounted(async () => {
+	const code = localStorage.getItem('pass');
+	if (code) return (securityCheckPassed.value = true);
+});
+async function handleAuth(pass: string) {
+	try {
+		if (pass !== '') {
+			const response = await (
+				await fetch(`${BASE_URL}/auth`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({ pass })
+				})
+			).json();
+
+			if (response === 'passed') {
+				securityCheckPassed.value = true;
+				localStorage.setItem('pass', Math.random().toString(36).slice(2));
+			} else {
+				authError.value = {
+					is: true,
+					message: 'Incorrect password'
+				};
+			}
+		}
+	} catch (error) {
+		authError.value = {
+			is: true,
+			message: 'Error ocured'
+		};
+	}
+}
 </script>
 
 <style lang="scss">
 @use '@/assets/sass/main.scss';
+
+.showUp-enter-active,
+.showUp-leave-active {
+	transition: opacity 400ms ease-in-out, transform 400ms ease-in-out;
+	transform: translateY(0px);
+}
+
+.showUp-enter-from,
+.showUp-leave-to {
+	opacity: 0;
+	transform: translateY(20px);
+}
 </style>
