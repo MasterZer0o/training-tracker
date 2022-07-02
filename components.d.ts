9b8385a96fcb5d3db93e8ca418fc97450f12a6c0
MasterZer0o <kubapod1234@wp.pl>
2022-07-02 23:35:57 +0200

    auto changes

--- components.d.ts
+++ components.d.ts
@@ -5,15 +5,17 @@ import '@vue/runtime-core'
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    AddButton: typeof import('./src/components/AddButton.vue')['default']
     AddSessionButton: typeof import('./src/components/AddSessionButton.vue')['default']
+    Error: typeof import('./src/components/Error.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    Headers: typeof import('./src/components/Headers.vue')['default']
     Loader: typeof import('./src/components/Loader.vue')['default']
     NewSession: typeof import('./src/components/NewSession.vue')['default']
+    RefreshButton: typeof import('./src/components/RefreshButton.vue')['default']
+    Security: typeof import('./src/components/Security.vue')['default']
     Session: typeof import('./src/components/Session.vue')['default']
     Sessions: typeof import('./src/components/Sessions.vue')['default']
-    Tracker: typeof import('./src/components/Tracker.vue')['default']
+    TheHeader: typeof import('./src/components/TheHeader.vue')['default']
   }
 }
 
