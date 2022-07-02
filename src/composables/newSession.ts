d9743e7b2f0731b4fb22a906999920a5a7d3b01b
MasterZer0o <kubapod1234@wp.pl>
2022-07-03 00:23:38 +0200

    update functionality

--- src/composables/newSession.ts
+++ src/composables/newSession.ts
@@ -2,25 +2,29 @@ import calculateDuration from './calculateDuration';
 import { Session } from './session';
 import { Ref } from 'vue';
 
-export function editNewContent({ target }: any): void {
-	if (target.hasAttribute('contenteditable')) {
-		target.textContent = '';
+export function editNewContent(e: any): void {
+	if (e.target.hasAttribute('contenteditable')) {
+		e.target.innerText = '';
 	} else return;
-
-	const observer = new MutationObserver(calculateDuration);
-	observer.observe(document.querySelector('.timeEnd'), { characterData: true, subtree: true });
+	window.scrollTo({ top: 0 });
+	const observer = new MutationObserver(() => calculateDuration(e.target));
+	e.target.classList.contains('date') || e.target.classList.contains('section')
+		? undefined
+		: observer.observe(e.target, { characterData: true, subtree: true });
 }
 
-export function addPlaceholder(sessions: Ref): void {
+export function addPlaceholder(sessions: Ref<Session[]>): void {
 	const sessionPlaceholder: Session = {
-		id: Math.floor(Math.random() * 100000),
-		section: 'insert part',
+		id: Date.now(),
+		section: 'part',
 		date: new Date().toLocaleDateString('pl'),
-		timeStart: 'start',
+		timeStart: `${new Date().getHours()}:${
+			new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes()
+		}`,
 		timeEnd: 'end',
 		duration: '',
 		isNew: true
 	};
- 
-	sessions.value = [sessionPlaceholder, ...sessions.value];
+
+	sessions.value.push(sessionPlaceholder);
 }
