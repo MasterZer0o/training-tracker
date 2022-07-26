93f3b67d75e2ffe35c6ef79f90c3e52542a0fbaa
MasterZer0o <kubapod1234@wp.pl>
2022-07-26 21:34:59 +0200

    refactor: conditional prefix for file path dev/prod

--- api/lib/getSessions.ts
+++ api/lib/getSessions.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
 import fs from 'fs/promises';
+import env from 'dotenv';
+env.config();
 
 export default async function getSessions(req: Request, res: Response) {
 	res.header('Access-Control-Allow-Origin', '*');
 	try {
-		const file = await fs.readFile('./api/sessions.json', { encoding: 'utf-8' });
+		const prefix = process.env.NODE_ENV === 'development' ? 'api' : 'server';
+
+		const file = await fs.readFile(`${prefix}/sessions.json`, { encoding: 'utf-8' });
 		res.json(JSON.parse(file)).status(200);
 	} catch (error) {
-		console.log(error);
+		console.error(error);
 
-		res.json({ error: { message: 'failed to retrieve sessions' } });
+		res.json({ error: { message: 'failed to retrieve sessions' } }).status(500);
 	}
 }
