10b331eea6213fba6e1e7d19beed93b15cadb6e0
MasterZer0o <kubapod1234@wp.pl>
2022-09-10 20:11:45 +0200

    add entry

--- src/cfg.ts
+++ src/cfg.ts
@@ -1,2 +1,3 @@
-export const BASE_URL: string = '';
+export const BASE_URL: string = 'http://192.168.0.22:5000';
 export const criticalErrorMessage: string = 'Something went wrong';
+export const ongoingCookieName: string = 'ongoing';
