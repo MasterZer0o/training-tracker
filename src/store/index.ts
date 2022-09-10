106af50444d0853db4e1a78afda859d508a2e20a
MasterZer0o <kubapod1234@wp.pl>
2022-09-10 20:12:42 +0200

    add ongoingSession state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,11 @@
 import { ref } from 'vue';
+import { OngoingSession } from '../../types';
 
 export const isLoading = ref<boolean>(true);
 export const isError = ref<boolean>(false); // controlling bottom box error
 export const errorMessage = ref<string>(''); // bottom box error message
+
+export const ongoingSession = ref<OngoingSession>({
+	is: false,
+	data: { section: '', started: '' }
+});
