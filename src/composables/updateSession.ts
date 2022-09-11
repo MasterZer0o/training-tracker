159ea7a8f1a360db51bffccbae256f92c8a2979e
MasterZer0o <kubapod1234@wp.pl>
2022-09-11 20:10:40 +0200

    add ongoing session functionality

--- src/composables/updateSession.ts
+++ src/composables/updateSession.ts
@@ -1,13 +1,15 @@
 import { Ref } from 'vue';
 import { BASE_URL } from '../cfg';
-import { isError, errorMessage } from '../store';
-import { Session } from './session';
+import { isError, errorMessage, ongoingSession } from '../store';
+import getOngoing from './ongoing/getOngoing';
+import resetOngoing from './ongoing/resetOngoing';
+import type { OngoingSession, Session } from '../../types';
+import setOngoing from './ongoing/setOngoing';
 
 export default async function updateSession(e: any, editing: Ref, loader: Ref, updateError: Ref, data: Session) {
 	try {
 		editing.value = null;
 		loader.value = true;
-		const path = `${BASE_URL}/editsession`;
 		const id = e.target.parentElement.parentElement.dataset.id;
 		const { section, date, timeStart, timeEnd, duration }: any = data;
 
@@ -21,7 +23,7 @@ export default async function updateSession(e: any, editing: Ref, loader: Ref, u
 		};
 
 		const response = await (
-			await fetch(path, {
+			await fetch(`${BASE_URL}/editsession`, {
 				method: 'POST',
 				headers: { 'Content-Type': 'application/json' },
 				body: JSON.stringify(session)
@@ -30,8 +32,26 @@ export default async function updateSession(e: any, editing: Ref, loader: Ref, u
 
 		if ('error' in response) throw response;
 
+		// session ended - reset.
+		if (session.timeEnd.length > 0) resetOngoing();
+		else {
+			const ongoing = getOngoing();
+			if (ongoing !== null) {
+				const ongoingData: OngoingSession = {
+					is: true,
+					data: {
+						section: section.value.querySelector('span').textContent,
+						started: ongoing.data.started
+					}
+				};
+				setOngoing(ongoingData);
+			}
+		}
+
 		loader.value = false;
 	} catch (error: any) {
+		console.error(error);
+
 		loader.value = false;
 		isError.value = true;
 		errorMessage.value = 'failed to edit session';
