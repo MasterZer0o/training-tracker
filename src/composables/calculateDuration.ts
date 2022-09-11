c7304de228bd7d2ddf62fd6d57f85a0e77647d9b
MasterZer0o <kubapod1234@wp.pl>
2022-09-11 20:12:13 +0200

    improve regex and rename variables

--- src/composables/calculateDuration.ts
+++ src/composables/calculateDuration.ts
@@ -1,38 +1,34 @@
 export default function calculateDuration(target: any) {
-	const regexp = /\d:\d{1,2}/g;
+	const timePattern = /^\d{1,2}:\d{1,2}$/; // ex. 13:22
 	const timeEndCell: Node = target.classList.contains('timeStart') ? target.nextSibling : target;
 	const durationCell: Node = timeEndCell.nextSibling;
 	const timeStartCell: Node = timeEndCell.previousSibling;
 
 	setTimeout(() => {
-		if (timeEndCell.textContent.match(regexp) && timeStartCell.textContent.match(regexp)) {
-			const dateNow: Date = new Date(Date.now());
-			const year: number = dateNow.getFullYear();
-			const month: number = dateNow.getMonth();
-			const day: number = dateNow.getDay();
+		if (timeEndCell.textContent.match(timePattern) && timeStartCell.textContent.match(timePattern)) {
+			const now: Date = new Date();
+			const yearNow: number = now.getFullYear();
+			const monthNow: number = now.getMonth();
+			const dayNow: number = now.getDay();
 
 			const timeStart = timeStartCell.textContent.split(':');
 			const timeStartHour: number = parseInt(timeStart[0]);
 			const timeStartMinutes: number = parseInt(timeStart[1]);
 
-			const timeEnd = timeEndCell.textContent.split(':');
-			const timeEndHour: number = parseInt(timeEnd[0]);
-			const timeEndMinutes: number = parseInt(timeEnd[1]);
+			const [timeEndHr, timeEndMin] = timeEndCell.textContent.split(':');
 
-			const d1: any = new Date(year, month, day, timeStartHour, timeStartMinutes, 0, 0);
-			const d2: any = new Date(year, month, day, timeEndHour, timeEndMinutes, 0, 0);
+			const timeEndHour: number = parseInt(timeEndHr);
+			const timeEndMinutes: number = parseInt(timeEndMin);
 
-			const difference = (d2 - d1) / 1000 / 60;
+			const date1: any = new Date(yearNow, monthNow, dayNow, timeStartHour, timeStartMinutes, 0, 0);
+			const date2: any = new Date(yearNow, monthNow, dayNow, timeEndHour, timeEndMinutes, 0, 0);
 
-			const differenceHours = Math.floor(difference / 60);
-			const differenceMinutes = difference % 60;
+			const timeDifference = (date2 - date1) / 1000 / 60;
 
-			durationCell.textContent =
-				differenceHours === 0 ? `${differenceMinutes}m` : `${differenceHours}h ${differenceMinutes}m`;
-		} else {
-			setTimeout(() => {
-				durationCell.textContent = 'invalid';
-			}, 500);
-		}
+			const differenceHours = Math.floor(timeDifference / 60);
+			const differenceMinutes = timeDifference % 60;
+
+			durationCell.textContent = differenceHours === 0 ? `${differenceMinutes}m` : `${differenceHours}h ${differenceMinutes}m`;
+		} else durationCell.textContent = 'invalid';
 	}, 500);
 }
