2a43d28bf99fe64904500ae666c768612ec28cde
MasterZer0o <kubapod1234@wp.pl>
2022-09-11 13:49:18 +0200

    refactor path reading

--- api/lib/getSessions.ts
+++ api/lib/getSessions.ts
@@ -6,13 +6,9 @@ env.config();
 export default async function getSessions(req: Request, res: Response) {
 	res.header('Access-Control-Allow-Origin', '*');
 	try {
-		const prefix = process.env.NODE_ENV === 'development' ? 'api' : 'server';
-
-		const file = await fs.readFile(`${prefix}/sessions.json`, { encoding: 'utf-8' });
+		const file = await fs.readFile(`${process.env.DB_FOLDER_NAME}/sessions.json`, { encoding: 'utf-8' });
 		res.json(JSON.parse(file)).status(200);
 	} catch (error) {
-		console.error(error);
-
 		res.json({ error: { message: 'failed to retrieve sessions' } }).status(500);
 	}
 }
