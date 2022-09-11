2ec905b8b3e13e86c5de14557c90f4621d492e4c
MasterZer0o <kubapod1234@wp.pl>
2022-09-11 13:47:23 +0200

    add helper function for getting db file

--- api/lib/addSession.ts
+++ api/lib/addSession.ts
@@ -1,29 +1,19 @@
-import { fileURLToPath } from 'url';
-import { Low, JSONFile } from 'lowdb';
-import { join, dirname } from 'path';
 import { Request, Response } from 'express';
-import type Session from '../Session';
+import type { Session } from '../../types';
+import getDb from './dbClient.js';
 
 export default async function addSession(req: Request, res: Response) {
 	try {
 		res.header('Access-Control-Allow-Origin', '*');
 
 		const session: Session = req.body;
+		const db = await getDb();
 
-		const __dirname = dirname(fileURLToPath(import.meta.url));
-
-		const file = join(__dirname, '../sessions.json');
-		const adapter = new JSONFile<any>(file);
-		const db = new Low(adapter);
-
-		await db.read();
-
-		db.data ||= [];
-
-		db.data.push(session);
-
-		await db.write();
-		res.json('success');
+		if (!('error' in db) && db.data !== null) {
+			db.data.push(session);
+			await db.write();
+			res.json('success');
+		} else throw 'Failed to add session.';
 	} catch (error) {
 		res.json({ error: { message: 'failed to add session' } });
 	}
