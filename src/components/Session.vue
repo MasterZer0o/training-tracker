eabf2ad2533224569507e4386ab7f020bd19f6c5
MasterZer0o <kubapod1234@wp.pl>
2022-09-11 20:16:34 +0200

    add ongoing session functionality, display dot on unfinished session

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -1,46 +1,23 @@
 <template>
-	<td :class="['section', editing ? 'editing-session' : '']" @dblclick="triggerSectionOptions" ref="section">
-		<span :contenteditable="isNew || editing" style="display: block; height: 100%; outline: none">
-			{{ session.section }}</span
-		>
+	<td :class="['section', editing ? 'editing-session' : null]" @dblclick="triggerSectionOptions" ref="section">
+		<span :contenteditable="isNew || editing" style="display: block; height: 100%; outline: none"> {{ session.section }}</span>
 		<transition name="slide">
-			<ul
-				@click="insertSection"
-				v-show="showSectionOptions"
-				class="section-options"
-				spellcheck="false"
-				contenteditable="false"
-			>
-				<li data-section="barki">barki</li>
-				<li data-section="klata">klata</li>
-				<li data-section="plecy">plecy</li>
-				<li data-section="nogi">nogi</li>
-			</ul>
+			<OptionsMenu @click="insertSection" v-show="showSectionOptions" />
 		</transition>
 	</td>
 
-	<td
-		class="date"
-		@click="editNewContent($event, isNew)"
-		@focus="editNewContent($event, isNew)"
-		ref="date"
-		:contenteditable="isNew || editing"
-	>
+	<td class="date" @click="_editNewContent" @focus="_editNewContent" ref="date" :contenteditable="isNew || editing">
 		{{ session.date }}
 	</td>
-	<td
-		class="timeStart"
-		@click="editNewContent($event, isNew)"
-		@focus="editNewContent($event, isNew)"
-		ref="timeStart"
-		:contenteditable="isNew || editing"
-	>
+	<td class="timeStart" @click="_editNewContent" @focus="_editNewContent" ref="timeStart" :contenteditable="isNew || editing">
 		{{ session.timeStart }}
 	</td>
 	<td
-		class="timeEnd"
-		@click="editNewContent($event, isNew)"
-		@focus="editNewContent($event, isNew)"
+		@input="showDotInput"
+		@focusout="showDotInput"
+		:class="['timeEnd', showDot ? 'dot' : null]"
+		@click="_editNewContent"
+		@focus="_editNewContent"
 		ref="timeEnd"
 		:contenteditable="isNew || editing"
 		@dblclick="insertTimeNow"
@@ -49,13 +26,7 @@
 	</td>
 	<td class="duration" ref="duration">{{ session.duration }}</td>
 	<td v-if="isNew">
-		<img
-			v-if="!loader && !hideSubmit"
-			@click="submitNewSession"
-			class="edit-icon"
-			src="../assets/checkbox.svg"
-			alt=""
-		/>
+		<img v-if="!loader && !hideSubmit" @click="submitNewSession" class="edit-icon" src="../assets/checkbox.svg" alt="" />
 		<img
 			v-if="hideSubmit && !loader && !updateError"
 			class="edit-icon"
@@ -72,22 +43,28 @@
 	<td v-else>
 		<img v-show="!loader && !updateError" class="edit-icon" @click="toggleEditing" src="../assets/edit.svg" alt="" />
 
-		<img v-if="editing" @click="_updateSession" class="edit-icon" src="../assets/checkbox.svg" alt="" />
+		<img v-if="editing" @click="_updateSession" class="edit-icon" src="../assets/checkbox.svg" alt="update session" />
 		<img v-if="updateError" src="../assets/alert.svg" class="alert-icon" alt="" />
 		<Loader v-if="loader" />
 	</td>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watchEffect } from 'vue';
 import { editNewContent } from '../composables/newSession';
 import updateSession from '../composables/updateSession';
-import type { Session } from '../composables/session';
+import type { OngoingSession, Session } from '../../types';
 import { isError, errorMessage } from '../store';
 import { BASE_URL } from '../cfg';
 import calculateDuration from '../composables/calculateDuration';
 import { computed } from '@vue/reactivity';
-
+import setOngoing from '../composables/ongoing/setOngoing';
+const showDot = ref(false);
+const props = defineProps<{
+	session: Session;
+	isNew: Boolean;
+}>();
+const showDotInput = (event: any) => (event.target?.innerText?.length > 0 ? (showDot.value = false) : (showDot.value = true));
 const editing = ref<null | true>(null);
 const loader = ref(false);
 const hideSubmit = ref(false);
@@ -101,19 +78,26 @@ const section = ref(null),
 const data: Session = { section, date, timeStart, timeEnd, duration };
 const _updateSession = (e: Event) => updateSession(e, editing, loader, updateError, data);
 
-function insertTimeNow(e: any) {
+const unWatchDot = watchEffect(() => {
+	showDot.value = timeEnd.value?.innerText.length === 0 ? true : false;
+	if (timeEnd.value !== null) unWatchDot();
+});
+
+function insertTimeNow(event: any) {
 	if (editing.value === true) {
-		e.target.textContent = `${new Date().getHours()}:${
+		showDot.value = false;
+		event.target.textContent = `${new Date().getHours()}:${
 			new Date().getMinutes() < 10 ? '0' + new Date().getMinutes() : new Date().getMinutes()
 		}`;
-		calculateDuration(e.target);
-		e.target.blur();
+		calculateDuration(event.target);
+		event.target.blur();
 	}
 }
-
+const _editNewContent = (e: any) => editNewContent(e, isNew.value, showDot);
 function insertSection(event: any) {
 	showSectionOptions.value = !showSectionOptions.value;
 	const sectionCell = section.value as HTMLElement;
+
 	sectionCell.querySelector('span').textContent = event.target.dataset.section;
 }
 
@@ -127,18 +111,20 @@ function toggleEditing(event: Event) {
 	event.stopPropagation();
 	if (editing.value == null) return (editing.value = true);
 	else {
+		if (timeEnd.value.innerText.length === 0) {
+			showDot.value = true;
+		}
 		editing.value = null;
 		showSectionOptions.value = false;
 	}
 }
-async function submitNewSession(e: any) {
-	const currentRow = e.target.closest('tr');
+async function submitNewSession(event: any) {
+	const currentRow = event.target.closest('tr');
 
 	try {
 		hideSubmit.value = true;
 		loader.value = true;
 
-		const path = `${BASE_URL}/addsession`;
 		const session: Session = {
 			id: currentRow.dataset.id,
 			section: section.value.querySelector('span').textContent,
@@ -148,13 +134,23 @@ async function submitNewSession(e: any) {
 			duration: duration.value.textContent
 		};
 
-		await fetch(path, {
+		await fetch(`${BASE_URL}/addsession`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(session)
 		});
+
+		const ongoingData: OngoingSession = {
+			is: true,
+			data: {
+				section: section.value.querySelector('span').textContent,
+				started: `${timeStart.value.textContent}:${new Date().getSeconds()}`
+			}
+		};
+		setOngoing(ongoingData);
+
 		loader.value = false;
 		editing.value = null;
 		isNew.value = false;
@@ -166,23 +162,15 @@ async function submitNewSession(e: any) {
 		updateError.value = true;
 	}
 }
-const props = defineProps<{
-	session: Session;
-	isNew: Boolean;
-}>();
+
 isNew.value = props.isNew ? true : false;
 onMounted(() => {
-	// section.value.focus()
-	props.session.isNew
-		? (() => {
-				const [...els]: any = document.querySelectorAll('.new-session td:first-child span');
+	if (props.session.isNew) {
+		const [...els]: any = document.querySelectorAll('.new-session td:first-child span');
 
-				const el: HTMLInputElement = els.pop();
-				setTimeout(() => {
-					el.focus();
-				}, 0);
-		  })()
-		: null;
+		const el: HTMLInputElement = els.pop();
+		setTimeout(() => el.focus(), 0);
+	}
 
 	// ensure new session row comes at the top
 	props.isNew
@@ -192,20 +180,3 @@ onMounted(() => {
 		: '';
 });
 </script>
-
-<style scoped>
-.spinner {
-	margin: 0;
-}
-.slide-enter-active,
-.slide-leave-active {
-	transition: opacity 200ms ease-in-out, transform 200ms ease-in-out;
-	transform: translateY(0px);
-}
-
-.slide-enter-from,
-.slide-leave-to {
-	opacity: 0;
-	transform: translateY(20px);
-}
-</style>
