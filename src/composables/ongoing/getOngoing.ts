d5ce61154fab40768143549fbdacb824ecc9934b
MasterZer0o <kubapod1234@wp.pl>
2022-09-12 16:44:31 +0200

    function for getting ongoing session data

---
+++ src/composables/ongoing/getOngoing.ts
@@ -0,0 +1,22 @@
+import { OngoingSession } from '../../../types';
+import { ongoingCookieName } from '../../cfg';
+export default function getOngoing(): OngoingSession | null {
+	try {
+		const cookies = document.cookie;
+		const cookieExists: boolean = cookies.includes('ongoing');
+		if (cookieExists) {
+			const cookie = cookies
+				.split(';')
+				.find((cookieString: string) => cookieString.includes(`${ongoingCookieName}`))
+				.split('=')[1];
+
+			const data = JSON.parse(cookie);
+
+			return data;
+		} else {
+			return null;
+		}
+	} catch (error) {
+		return null;
+	}
+}
