93cdf10453b83f42c8eee63739cd16f5b49ed778
MasterZer0o <kubapod1234@wp.pl>
2022-09-13 21:13:16 +0200

    update

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
     Error: typeof import('./src/components/Error.vue')['default']
     Headers: typeof import('./src/components/Headers.vue')['default']
     Loader: typeof import('./src/components/Loader.vue')['default']
+    NetDisconnect: typeof import('./src/components/NetDisconnect.vue')['default']
     OngoingSession: typeof import('./src/components/OngoingSession.vue')['default']
     OptionsMenu: typeof import('./src/components/OptionsMenu.vue')['default']
     RefreshButton: typeof import('./src/components/RefreshButton.vue')['default']
